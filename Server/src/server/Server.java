90332e469d26b2547b0a362b48dc95b0c412cc1e
Mostafa-PC <>
2019-12-13 18:29:27 +0200

    3mlt cout fl server kiko tany

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -15,7 +15,7 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  
+        System.out.println("The project started, let the game begin!");
         System.out.println("KIKOO IS HEREE ");
         System.out.println("Nazzz is here");
         System.out.println("GHUNGHUN");
